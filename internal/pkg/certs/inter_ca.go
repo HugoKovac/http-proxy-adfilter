1d71519e4ea6e7b572f7fd42edf5f836f61caaa7
h.kovac <h.kovac@eyeo.com>
2024-10-14 11:00:58 +0200

    rm debug time

--- internal/pkg/certs/inter_ca.go
+++ internal/pkg/certs/inter_ca.go
@@ -21,7 +21,6 @@ func (c *Cert) intermediateCa(domain string) (*tls.Certificate, error) {
 			log.Printf("Generated certificate for domain: %s", domain)
 		}
 	}()
-	now := time.Now()
 	cert := x509.Certificate{
 		SerialNumber: big.NewInt(time.Now().Unix()),
 		Subject: pkix.Name{
@@ -35,51 +34,40 @@ func (c *Cert) intermediateCa(domain string) (*tls.Certificate, error) {
 		BasicConstraintsValid: true,
 		DNSNames:              []string{domain},
 	}
-	log.Println("> cert: ", time.Since(now))
 
-	now = time.Now()
 	// privateKey, err := rsa.GenerateKey(rand.Reader, 2048) // performance issue
 	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	if err != nil {
 		return nil, err
 	}
-	log.Println("> key: ", time.Since(now))
 
 
 	// Sign the certificate with the CA
-	now = time.Now()
 	certDER, err := x509.CreateCertificate(rand.Reader, &cert, &c.RootCa, &privateKey.PublicKey, c.rootKey)
 	if err != nil {
 		return nil, err
 	}
-	log.Println("> DER: ", time.Since(now))
 
 
 	// PEM encode the certificate and private key
-	now = time.Now()
 	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
 	der, err := x509.MarshalPKCS8PrivateKey(privateKey)
 	if err != nil {
 		return nil, err
 	}
 	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: der})
-	log.Println("> pem: ", time.Since(now))
 
 
 	// Create a tls.Certificate to use in tls.Config
-	now = time.Now()
 	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
 	if err != nil {
 		return nil, err
 	}
-	log.Println("> tls: ", time.Since(now))
 
 	// Cache the certificate
-	now = time.Now()
 	c.Mu.Lock()
 	c.CertCache[domain] = &tlsCert
 	c.Mu.Unlock()
-	log.Println("> store: ", time.Since(now))
 
 	return &tlsCert, nil
 }
